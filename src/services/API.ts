66db3f0bd1a2ab876b526b39280d33d6b541c2d6
Alexandr Loginov <atatakobry@gmail.com>
2020-08-26 17:51:32 +0300

    update `API` service

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,29 +1,36 @@
 import axios from 'axios';
 import { compact, transform } from 'lodash';
 
-const fetchIngredients = () => {
-  interface IResponse {
-    data: {
-      drinks: Array<IResponseItem>;
-    };
-  }
-  interface IResponseItem {
-    strIngredient1: string;
-  }
+const host = 'the-cocktail-db.p.rapidapi.com';
+const key = '26f7bfa6eemsh64d29e32a9df91ap1ac7f1jsn1ef195db90ed';
+const url = `https://${host}`;
 
+interface IResponse<T> {
+  data: T;
+}
+interface IDrinks<T> {
+  drinks: Array<T>;
+}
+interface IIngredient {
+  strIngredient1: string;
+}
+interface IGlass {
+  strGlass: string;
+}
+
+const fetchIngredients = () => {
   return axios
-    .get('https://the-cocktail-db.p.rapidapi.com/list.php?i=list', {
+    .get(`${url}/list.php?i=list`, {
       headers: {
-        'x-rapidapi-host': 'the-cocktail-db.p.rapidapi.com',
-        'x-rapidapi-key': '26f7bfa6eemsh64d29e32a9df91ap1ac7f1jsn1ef195db90ed',
+        'x-rapidapi-host': host,
+        'x-rapidapi-key': key,
       },
     })
-    .then((response: IResponse) =>
+    .then((response: IResponse<IDrinks<IIngredient>>) =>
       compact(
         transform(
           response.data.drinks,
-          (ingredients: Array<string>, item: IResponseItem) =>
-            ingredients.push(item.strIngredient1),
+          (ingredients: Array<string>, item: IIngredient) => ingredients.push(item.strIngredient1),
           []
         )
       )
@@ -31,28 +38,18 @@ const fetchIngredients = () => {
 };
 
 const fetchGlasses = () => {
-  interface IResponse {
-    data: {
-      drinks: Array<IResponseItem>;
-    };
-  }
-  interface IResponseItem {
-    strGlass: string;
-  }
-
   return axios
-    .get('https://the-cocktail-db.p.rapidapi.com/list.php?g=list', {
+    .get(`${url}/list.php?g=list`, {
       headers: {
-        'x-rapidapi-host': 'the-cocktail-db.p.rapidapi.com',
-        'x-rapidapi-key': '26f7bfa6eemsh64d29e32a9df91ap1ac7f1jsn1ef195db90ed',
+        'x-rapidapi-host': host,
+        'x-rapidapi-key': key,
       },
     })
-    .then((response: IResponse) =>
+    .then((response: IResponse<IDrinks<IGlass>>) =>
       compact(
         transform(
           response.data.drinks,
-          (ingredients: Array<string>, item: IResponseItem) =>
-            ingredients.push(item.strGlass),
+          (ingredients: Array<string>, item: IGlass) => ingredients.push(item.strGlass),
           []
         )
       )
